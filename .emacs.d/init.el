0ce5bf9ed3121215c9c294a8c408f53ae386957d
Nick Kirby <33106137+Kirizan@users.noreply.github.com>
2022-09-30 12:08:23 -0400

    Moved temporary files to keep .emacs.d clean. Changed user directory to keep .emacs.d clean.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -227,6 +227,19 @@
 (kirby/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
 
+;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
+      url-history-file (expand-file-name "url/history" user-emacs-directory))
+
+;; Use no-littering to automatically set common paths to the new user-emacs-directory
+(use-package no-littering)
+
+;; Keep customization settings in a temporary file (thanks Ambrevar!)
+(setq custom-file
+      (if (boundp 'server-socket-dir)
+          (expand-file-name "custom.el" server-socket-dir)
+        (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
+(load custom-file t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
