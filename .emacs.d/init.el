95b2991aa6104fec115dd28c07bb5a60c6e8cba6
Nick Kirby <33106137+Kirizan@users.noreply.github.com>
2022-10-12 12:02:35 -0400

    Disabled warning about second line in commit message.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -344,6 +344,9 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(setq git-commit-style-convention-checks
+      (remove 'non-empty-second-line git-commit-style-convention-checks))
+
 (use-package forge
   :init
   (setq auth-sources '("~/.authinfo")))
