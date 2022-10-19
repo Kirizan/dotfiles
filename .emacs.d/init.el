9d2c5685ce7e486e55f30f57a04e2ddc8361b067
Nick Kirby <33106137+Kirizan@users.noreply.github.com>
2022-10-19 13:06:42 -0400

    Added initial eshell configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -247,6 +247,36 @@
   (setq vterm-shell "zsh")
   (setq vterm-max-scrollback 10000))
 
+(defun kirby/configure-eshell ()
+  ;; Save command history when commands are entered
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+  ;; Truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+  ;; Bind some useful keys for evil-mode
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
+  (evil-normalize-keymaps)
+
+  (setq eshell-history-size         10000
+        eshell-buffer-maximum-lines 10000
+        eshell-hist-ignoredups t
+        eshell-scroll-to-bottom-on-input t))
+
+(use-package eshell
+  :hook (eshell-first-time-mode . kirby/configure-eshell)
+  :config
+
+  (with-eval-after-load 'esh-opt
+    (setq eshell-destroy-buffer-when-process-dies t)
+    (setq eshell-visual-commands '("htop" "zsh" "vim")))
+
+  (eshell-git-prompt-use-theme 'powerline))
+
+(use-package eshell-git-prompt
+  :after eshell)
+
 (defun kirby/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
