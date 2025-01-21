439c3bee75762893f261d16dc8b35154cf5f07f9
Nick Kirby <33106137+Kirizan@users.noreply.github.com>
2025-01-21 12:41:53 -0500

    Added chemacs2 config file

---
+++ .emacs-profiles.el
@@ -0,0 +1,9 @@
+(
+ ("default" . ((user-emacs-directory . "~/.emacs.default")))
+
+ ("doom"  . ((user-emacs-directory . "~/.config/emacs-doom")
+             (env . (("DOOMDIR" . "~/.config/doom")))))
+  
+ ("spacemacs" . ((user-emacs-directory . "~/.config/emacs-spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.config/spacemacs")))))
+ )
